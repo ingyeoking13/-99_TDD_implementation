d5db71a6531a1559ae33e9d134f214ecb53b3e62
Jeong Yo Han <ingyeoking13@gmail.com>
2024-03-22 14:02:05 +0900

    refactor-test-code: pointcontroller -> pointservice

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -13,9 +13,8 @@ import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PointControllerTest {
+class PointServiceTest {
 
-    private PointController pointController;
     private UserPointTable userPointTable;
     private PointHistoryTable pointHistoryTable;
     private PointService pointService;
@@ -25,14 +24,13 @@ class PointControllerTest {
         userPointTable = new UserPointTable();
         pointHistoryTable = new PointHistoryTable();
         pointService = new PointService(userPointTable, pointHistoryTable);
-        pointController = new PointController(pointService);
     }
 
 
     @Test
     public void test_신규_유저_포인트_조회(){
         // given-when
-        UserPoint usersPoint = pointController.point(1);
+        UserPoint usersPoint = pointService.getPointById(1);
         // then
         assertEquals(0L, usersPoint.point());
     }
@@ -40,7 +38,7 @@ class PointControllerTest {
     @Test
     public void test_유저가_충전한다(){
         // given-when
-        UserPoint result = pointController.charge(1, 1000);
+        UserPoint result = pointService.charge(1, 1000);
         // then
         assertEquals(1000L, result.point());
     }
@@ -48,9 +46,9 @@ class PointControllerTest {
     @Test
     public void test_충전_유저_포인트_조회(){
         // given
-        pointController.charge(1, 1000);
+        pointService.charge(1, 1000);
         // when
-        UserPoint usersPoint = pointController.point(1);
+        UserPoint usersPoint = pointService.getPointById(1);
         // then
         assertEquals(1000L, usersPoint.point());
     }
@@ -58,10 +56,10 @@ class PointControllerTest {
     @Test
     public void test_충전_유저_포인트_조회_2(){
         // given
-        pointController.charge(1, 1000);
-        pointController.charge(1, 2000);
+        pointService.charge(1, 1000);
+        pointService.charge(1, 2000);
         // when
-        UserPoint usersPoint = pointController.point(1);
+        UserPoint usersPoint = pointService.getPointById(1);
         // then
         assertEquals(3000L, usersPoint.point());
     }
@@ -69,33 +67,33 @@ class PointControllerTest {
     @Test
     public void test_신규_유저가_포인트를_사용한다(){
         // given-when-then
-        assertThrows(MinusPointException.class, () -> pointController.use(1, 1000));
+        assertThrows(MinusPointException.class, () -> pointService.use(1, 1000));
     }
 
     @Test
     public void test_충전유저가_포인트를_사용한다(){
         // given
-        pointController.charge(1, 1000);
+        pointService.charge(1, 1000);
         // when
-        pointController.use(1, 300);
+        pointService.use(1, 300);
         // then
-        UserPoint point = pointController.point(1);
+        UserPoint point = pointService.getPointById(1);
         assertEquals(700L, point.point());
     }
 
     @Test
     public void test_충전유저가_과도한_포인트를_사용한다(){
         // given
-        pointController.charge(1, 1000);
+        pointService.charge(1, 1000);
         // when-then
-        assertThrows(MinusPointException.class, () -> pointController.use(1, 1100));
+        assertThrows(MinusPointException.class, () -> pointService.use(1, 1100));
     }
 
 
     @Test
     public void test_미사용유저의_히스토리를_가져온다(){
         // given-when
-        List<PointHistory> pointHistories = pointController.history(1);
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(1);
 
         // then
         assertTrue(pointHistories.isEmpty());
@@ -104,10 +102,10 @@ class PointControllerTest {
     @Test
     public void test_사용유저의_히스토리를_가져온다() {
         // given
-        pointController.charge(1, 1000);
-        pointController.use(1, 700);
+        pointService.charge(1, 1000);
+        pointService.use(1, 700);
         // when
-        List<PointHistory> pointHistories = pointController.history(1);
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(1);
         // then
         assertEquals(TransactionType.CHARGE, pointHistories.get(0).type());
         assertEquals(1000L, pointHistories.get(0).amount());
@@ -125,20 +123,20 @@ class PointControllerTest {
         // when
         for (int i=0; i<numThreads; i++) {
             executorService.execute(() -> {
-                pointController.charge(1, 100);
+                pointService.charge(1, 100);
                 doneSignal.countDown();
             });
         }
         doneSignal.await();
-        List<PointHistory> pointHistories = pointController.history(1);
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(1);
         assertEquals(10, pointHistories.size());
-        assertEquals(1000, pointController.point(1).point());
+        assertEquals(1000, pointService.getPointById(1).point());
     }
 
     @Test
     public void test_동시성환경에서_유저가_사용을_수행한다() throws InterruptedException {
         // given
-        pointController.charge(1, 1000);
+        pointService.charge(1, 1000);
 
         int numThreads = 10;
         ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
@@ -147,15 +145,15 @@ class PointControllerTest {
         // when
         for (int i=0; i<numThreads; i++) {
             executorService.execute(() -> {
-                pointController.use(1, 100);
+                pointService.use(1, 100);
                 doneSignal.countDown();
             });
         }
         doneSignal.await();
 
-        List<PointHistory> pointHistories = pointController.history(1);
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(1);
         assertEquals(11, pointHistories.size());
-        assertEquals(0, pointController.point(1).point());
+        assertEquals(0, pointService.getPointById(1).point());
     }
 
 
